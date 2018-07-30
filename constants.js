63483aef549cb685686c18f35263f2e1ae4a3053
Ben Demboski <ben@turbopatent.com>
2018-07-29 22:42:34 -0700

    Initial commit

---
+++ constants.js
@@ -0,0 +1,23 @@
+//ddb table name
+exports.HOST_TABLE_NAME = "Host";
+exports.CASE_WORKER_TABLE_NAME = "CaseWorker";
+exports.RESIDENT_TABLE_NAME = "Resident";
+
+//ddb item
+exports.item = "Item";
+exports.items = "items";
+exports.total = "TOTAL";
+
+//ddb item keys
+exports.email = "email";
+exports.password = "password";
+exports.agency = "agency";
+exports.fname = "first_name";
+exports.lname = "last_name";
+exports.phone = "phone";
+exports.residents = "residents";
+exports.CaseWorker = "case_worker";
+
+//general
+exports.succeeded = "succeeded";
+exports.logout = "logout";
