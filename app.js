68ba0fb9b68d57d845dbf6f71bb1ef9177512835
Ben Demboski <ben@turbopatent.com>
2018-07-31 22:55:34 -0700

    Fix CORS support and Authorization header parsing

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const sls = require('serverless-http');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const { check, validationResult } = require('express-validator/check');
 const asyncHandler = require('express-async-handler');
 
@@ -13,6 +14,7 @@ const AWS = require('aws-sdk');
 let app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 useCognitoAuth(app);
 
 app.post('/createUser', [
