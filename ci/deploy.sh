b1e9188ebddfcb1a96da10bf6f2dde06e904c08a
Ben Demboski <ben@turbopatent.com>
2019-02-05 23:08:32 -0800

    Deploy to both dev and stage

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -3,5 +3,7 @@
 echo "decrypting credentials..."
 mkdir -p ~/.aws
 openssl aes-256-cbc -K $encrypted_a11d45259c2e_key -iv $encrypted_a11d45259c2e_iv -in ci/aws-credentials.enc -out ~/.aws/credentials -d
-echo "deploying..."
-yarn deploy $*
+echo "deploying to dev..."
+yarn deploy -s dev
+echo "deploying to stage..."
+yarn deploy -s stage
